59c02e16bf9945d52d5cbb33dcbcfa0d0b25cb89
Jannis Graef <jannisg2912gmail.com>
2024-01-23 13:21:45 +0100

    fertig

--- ws/feiertage.php
+++ ws/feiertage.php
@@ -24,10 +24,23 @@ date_default_timezone_set('Europe/Berlin');
 $dtEaster = (new DateTime())->setTimestamp(easter_date(date('Y')));
 
 foreach ($aHolidayList as $dateExpr => $desc) {
-    $dtCurr = strpos($dateExpr, 'E') === 0
-        ? (clone $dtEaster)->modify(ltrim($dateExpr, 'E').' day')
-        : new DateTime($dateExpr.date('Y'));
-
+    $dtCurr = strpos($dateExpr, 'E') === 01.01.' => 'Neujahr',
+    '06.01.' => 'Hl. drei Könige',
+    'E-1'    => 'Test -1',
+    'E+0'    => 'Ostersonntag',
+    'E+1'    => 'Ostermontag',
+    '01.05.' => 'Staatsfeiertag',
+    'E+39'   => 'Christi Himmelfahrt',
+    'E+50'   => 'Pfingstmontag',
+    'E+60'   => 'Fronleichnam',
+    '15.08.' => 'Maria Himmelfahrt',
+    '26.10.' => 'Nationalfeiertag',
+    '01.11.' => 'Allerheiligen',
+    '08.12.' => 'Maria Empfängnis',
+    '24.12.' => 'Heilig Abend',
+    '25.12.' => 'Christtag',
+    '26.12.' => 'Stefanitag',
+    '31.12.' => 'Silvester'
     echo $dtCurr->format('d.m.Y') . " -- " . $desc . "<br>";
 }
 ?>
