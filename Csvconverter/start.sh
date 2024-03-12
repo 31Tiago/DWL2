dddef96840e88b6e2d7d647eb908bebfea93a95e
tiago <tiago.zeppmart@gmail.com>
2024-03-12 08:50:19 +0100

    feritg

--- Csvconverter/start.sh
+++ Csvconverter/start.sh
@@ -3,7 +3,7 @@
 /usr/local/bin/python /app/csv_to_db.py
 
 # Füge den Cron-Job für alle 12 Stunden hinzu und leite die Ausgabe in eine Datei
-echo "* */12 * * * root /usr/local/bin/python /app/csv_to_db.py > /app/data/cron.log 2>&1" > /etc/cron.d/csv-cron
+echo "* */12 * * * root /usr/local/bin/python /app/csv_to_db.py >> /app/data/cron.log 2>&1" > /etc/cron.d/csv-cron
 
 # Starte den Cron-Daemon
 cron && tail -f /app/data/cron.log
